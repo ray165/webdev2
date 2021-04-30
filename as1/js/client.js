395cb89ff82e4a4b8f9ce5f5bb34dbd461256905
Raymond <70726900+ray165@users.noreply.github.com>
2021-04-30 01:54:20 -0700

    dom card function created in client js

--- as1/js/client.js
+++ as1/js/client.js
@@ -16,3 +16,24 @@ $(document).ready(function () {
     },
   });
 });
+
+
+function buildCard(data){
+    var card = document.createElement("div");
+    card.className = "card";
+    var cardBody = document.createElement("div");
+    cardBody.className =  "card-body";
+    var cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    var cardSubTitle = document.createElement("h6");
+    cardSubTitle.className = "card-subtitle mb-2 text-muted";
+    var cardText = document.createElement("p");
+    cardText.className = "card-text";
+
+    // assign data.
+    
+
+    cardBody.append(cardTitle, cardSubTitle, cardText);
+    card.append(cardBody);
+    document.querySelector("#root").append(card);
+}
